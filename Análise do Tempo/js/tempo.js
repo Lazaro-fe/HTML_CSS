0cb88900645b2ef7a4790164178f2d5d3393d856
Lázaro <155861352+Lazaro-fe@users.noreply.github.com>
2025-11-26 19:10:11 +0000

    Salvando

---
+++ Análise do Tempo/js/tempo.js
@@ -0,0 +1,16 @@
+// DOM
+const cidade = document.querySelector('#cidade')
+const bprocurar = document.querySelector('#bprocurar')
+const city = document.querySelector('#city')
+const temperatura = document.querySelector('#bprocurar')
+const previsao = document.querySelector('#previsao')
+const chave = 'e12a59aeb26f36a9ee4d7eba3588239c'
+
+// EVENTO
+
+bprocurar.addEventListener('click', buscarcidade)
+
+
+
+
+// FUNÇÃO
