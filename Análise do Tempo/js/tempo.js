81525673521f53437020be2f66b3b791d049d154
Lázaro <155861352+Lazaro-fe@users.noreply.github.com>
2025-12-01 19:15:08 +0000

    Salvando

--- Análise do Tempo/js/tempo.js
+++ Análise do Tempo/js/tempo.js
@@ -10,7 +10,18 @@ const chave = 'e12a59aeb26f36a9ee4d7eba3588239c'
 
 bprocurar.addEventListener('click', buscarcidade)
 
-
-
-
 // FUNÇÃO
+
+/* Função assíncrona é uma função que não bloqueia a execução de outras tarefas */
+async function buscarcidade(){
+
+    cidade_valor = cidade.value
+    
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade_valor}&appid=${chave}`)
+
+    console.log(dados)
+
+    city.textContent = `Previsão de ${dados.name}`
+    temperatura.textContent = `${(dados.main.temp).toFixed(0)}°C`
+    previsao.textContent = `${dados.weather[0].description}`
+}
